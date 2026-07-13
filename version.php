7954561de0c8dfebb5d7027f7a10c3dfd75e34f8
Edugital <contato@edugital.com.br>
2026-07-13 17:51:08 -0300

    feat(calibration): bump human criterion review version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026071302;
+$plugin->version = 2026071303;
 $plugin->requires = 2024042200;
 $plugin->component = 'assignfeedback_aitutoria';
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.3.0-alpha.1';
+$plugin->release = '0.3.0-alpha.2';
