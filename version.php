45d81e4243064451e712f7abd1d177b1b304c8d9
Edugital <contato@edugital.com.br>
2026-07-13 16:10:32 -0300

    fix(moodle): align version metadata and boilerplate

--- version.php
+++ version.php
@@ -5,6 +5,14 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Version information for the AI Tutoring assignment feedback plugin.
@@ -16,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026071300;
+$plugin->version = 2026071301;
 $plugin->requires = 2024042200;
 $plugin->component = 'assignfeedback_aitutoria';
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.2.0-recovery';
+$plugin->release = '0.2.1-recovery';
