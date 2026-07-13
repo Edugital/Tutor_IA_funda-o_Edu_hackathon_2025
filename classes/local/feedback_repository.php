31fb55ec729ba73bf6b973d9594de0299b4a80ff
Edugital <contato@edugital.com.br>
2026-07-13 16:11:52 -0300

    fix(moodle): normalize feedback repository and table name

--- classes/local/feedback_repository.php
+++ classes/local/feedback_repository.php
@@ -5,10 +5,16 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 
 namespace assignfeedback_aitutoria\local;
-
-defined('MOODLE_INTERNAL') || die();
 
 /**
  * Persistence boundary for AI Tutoring feedback.
@@ -18,9 +24,8 @@ defined('MOODLE_INTERNAL') || die();
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 final class feedback_repository {
-
     /** Database table. */
-    private const TABLE = 'assignfeedback_aitut_fb';
+    private const TABLE = 'assignfeedback_aitutoria';
 
     /**
      * Fetch a record by Moodle assignment grade id.
