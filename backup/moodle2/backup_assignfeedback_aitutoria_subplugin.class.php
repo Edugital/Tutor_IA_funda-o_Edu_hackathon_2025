e1c36c40c61fbd355c19cc4240bef1a64e08cb5c
Edugital <contato@edugital.com.br>
2026-07-13 15:56:55 -0300

    chore(migration): add Moodle backup support

---
+++ backup/moodle2/backup_assignfeedback_aitutoria_subplugin.class.php
@@ -0,0 +1,60 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * Backup support for AI Tutoring assignment feedback.
+ *
+ * @package    assignfeedback_aitutoria
+ * @copyright  2026 Edugital
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Defines grade-level backup data for the subplugin.
+ */
+class backup_assignfeedback_aitutoria_subplugin extends backup_subplugin {
+
+    /**
+     * Define feedback records attached to an assignment grade.
+     *
+     * @return backup_subplugin_element
+     */
+    protected function define_grade_subplugin_structure() {
+        $subplugin = $this->get_subplugin_element();
+        $wrapper = new backup_nested_element($this->get_recommended_name());
+        $record = new backup_nested_element(
+            'feedback_aitutoria',
+            null,
+            [
+                'grade',
+                'feedbacktext',
+                'feedbackformat',
+                'aisuggestion',
+                'aistatus',
+                'decision',
+                'model',
+                'promptversion',
+                'rubricversion',
+                'timecreated',
+                'timemodified',
+            ]
+        );
+
+        $subplugin->add_child($wrapper);
+        $wrapper->add_child($record);
+
+        $record->set_source_table(
+            'assignfeedback_aitut_fb',
+            ['grade' => backup::VAR_PARENTID]
+        );
+
+        return $subplugin;
+    }
+}
