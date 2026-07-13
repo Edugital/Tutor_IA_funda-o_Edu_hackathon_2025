7f03bab08c828dc92c76ee4b2be8dc2c90ac7e03
Edugital <contato@edugital.com.br>
2026-07-13 16:11:35 -0300

    fix(moodle): normalize HIC policy class

--- classes/local/decision_policy.php
+++ classes/local/decision_policy.php
@@ -5,10 +5,16 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 
 namespace assignfeedback_aitutoria\local;
-
-defined('MOODLE_INTERNAL') || die();
 
 /**
  * Deterministic human-in-control decision policy.
@@ -21,7 +27,6 @@ defined('MOODLE_INTERNAL') || die();
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 final class decision_policy {
-
     /**
      * Resolve the feedback to publish.
      *
