6bc7792414e9974df01ff3719f2923a595eb7e7e
Edugital <contato@edugital.com.br>
2026-07-13 16:13:33 -0300

    fix(moodle): normalize assignment feedback implementation

--- locallib.php
+++ locallib.php
@@ -5,6 +5,14 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Assignment feedback implementation for AI Tutoring.
@@ -13,8 +21,6 @@
  * @copyright  2026 Edugital
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-defined('MOODLE_INTERNAL') || die();
 
 use assignfeedback_aitutoria\local\feedback_repository;
 
@@ -26,7 +32,6 @@ use assignfeedback_aitutoria\local\feedback_repository;
  * layer, remain unpublished until accepted or edited by the grader.
  */
 class assign_feedback_aitutoria extends assign_feedback_plugin {
-
     /**
      * Plugin display name.
      *
