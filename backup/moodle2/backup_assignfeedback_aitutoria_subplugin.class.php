4dfd892c92d59ee36cd1fbe8660a17583dc9f0e7
Edugital <contato@edugital.com.br>
2026-07-13 16:12:27 -0300

    fix(moodle): normalize backup implementation

--- backup/moodle2/backup_assignfeedback_aitutoria_subplugin.class.php
+++ backup/moodle2/backup_assignfeedback_aitutoria_subplugin.class.php
@@ -5,6 +5,14 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Backup support for AI Tutoring assignment feedback.
@@ -14,13 +22,10 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Defines grade-level backup data for the subplugin.
  */
 class backup_assignfeedback_aitutoria_subplugin extends backup_subplugin {
-
     /**
      * Define feedback records attached to an assignment grade.
      *
@@ -51,7 +56,7 @@ class backup_assignfeedback_aitutoria_subplugin extends backup_subplugin {
         $wrapper->add_child($record);
 
         $record->set_source_table(
-            'assignfeedback_aitut_fb',
+            'assignfeedback_aitutoria',
             ['grade' => backup::VAR_PARENTID]
         );
 
